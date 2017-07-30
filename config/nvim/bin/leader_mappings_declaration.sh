45c006e78ab56d2678a2bc6a40fc3c141654b670
David <davidsu@wix.com>
2017-07-30 16:17:43 +0300

    include <C-\w> maps to leader_mappings_declaration

--- config/nvim/bin/leader_mappings_declaration.sh
+++ config/nvim/bin/leader_mappings_declaration.sh
@@ -1,5 +1,5 @@
 ag --color --color-match "38;05;1" --nogroup --ignore 'plugged' --ignore 'autoload/plug*' \
-    '^[^\"\n]*map.*\s(<space>\w\w\w? |\\[\w\\] |1\w ).*?\w' | \
-    perl -pe 's#([^\"\n]*?:.*?map.*?(<space>\w\w\w?|\\[\w\\]|1\w) .*)#\2   \1#g' | \
-    ag '(^<space>...?|1.|\\.) ' | sort 
+    '^[^\"\n]*map.*\s(<space>\w\w\w? |\\[\w\\] |1\w |<[cC]-\w>).*?\w' | \
+    perl -pe 's#([^\"\n]*?:.*?map.*?(<space>\w\w\w?|\\[\w\\]|1\w|<[cC]-[^\s]*?) .*)#\2   \1#g' | \
+    ag '^(<space>...?|1.|\\.|<c-.*) ' | sort 
 
