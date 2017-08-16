7e7ab447f47fb92d432852f27468f094113c3795
David <davidsu@wix.com>
2017-08-16 13:40:29 +0300

    --no-sort for fj cuz fasd's frecency is awesome

--- fzf.zsh
+++ fzf.zsh
@@ -88,7 +88,7 @@ FZF-EOF"
 alias chromebookmarks='$DOTFILES/fzf/chromebookmarks.rb'
 
 function jfzf(){
-  dir=`fasd_cd -ds | egrep '^\S+\s+/' | tail -r | fzf`
+  dir=`fasd_cd -ds | egrep '^\S+\s+/' | tail -r | fzf --no-sort --bind 'ctrl-s:toggle-sort'`
   if [[ -n dir ]]; then
     cd $(echo $dir | sed -E 's#[^/]*(/.*$)#''\1''#')
   fi
