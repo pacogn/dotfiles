04e01981afbed10d2d8a09ee0f0320265137750d
David <davidsu@wix.com>
2018-05-09 08:21:03 +0300

    improved fzfgitshow (wasn't working on santa-editor)

--- config/nvim/bin/fzfgitshow.sh
+++ config/nvim/bin/fzfgitshow.sh
@@ -1,1 +1,3 @@
-git show --color --pretty=oneline $(sed -E 's#.*([0-9a-f]{7}).*#\1#' <<< $1)
+#!/bin/bash
+echo $(sed -E 's#.*([0-9a-f]{7}).*#\1#' <<< $1) > /tmp/a
+git show --color --pretty=oneline $(perl -pe 's#.*?([0-9a-f]+).*#\1#' <<< $1)
